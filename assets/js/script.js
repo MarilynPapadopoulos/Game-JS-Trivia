918c5fbb6a96fe0c8b1dcc8d8ddc0de4275b992b
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-03-19 20:07:30 -0400

    end game, check answer functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,25 @@
 var timeLeft = 60;
 var score=0;
 
+var questionContainer = document.querySelector("#question");
+
+var aOne = document.querySelector("#a1");
+var aTwo = document.querySelector("#a2");
+var aThree = document.querySelector("#a3");
+var aFour =  document.querySelector("#a4");
+
+
+var questionBox = [
+    {q:"This is the first question.", choices:["1", "2", "3","4"], a: "1"},
+    {q:"This is the second question.", choices:["11", "22", "33","44"], a: "22"},
+    {q:"This is the third question.", choices:["5", "6", "7","8"], a: "7"},
+    {q:"This is the fourth question.", choices:["10", "20", "30","40"], a: "40"}
+];
+ var questionIndex = 0;  
+
 //create function to start game and timer
 function startGame() {
-    var startEl = document.querySelector("#start-button");
-    startEl.addEventListener("click", function() {
+   
     
         var timeEl = document.getElementById("timer");
         var timerId = setInterval(function() {
@@ -18,69 +33,73 @@ function startGame() {
         
             console.log(timeLeft);
          },1000)
-    })
+    
+    question();
 };
-startGame();
 
+var startEl = document.querySelector("#start-button");
+startEl.addEventListener("click", startGame);
    
-    aOne = document.querySelector("#a1");
-    aTwo = document.querySelector("#a2");
-    aThree = document.querySelector("#a3");
-    aFour =  document.querySelector("#a4");
-
-    var questionBox = document.querySelector("#container");
-    questionBox = [
-        {q:"This is the first question.", choices:["1", "2", "3","4"], a: "1"},
-        {q:"This is the second question.", choices:["11", "22", "33","44"], a: "22"},
-        {q:"This is the third question.",choices:["5", "6", "7","8"], a: "7"},
-        {q:"This is the fourth question.",choices:["10", "20", "30","40"], a: "40"}
-    ];
-        
+   
     function question() {
 
-        var clicked = event.target;
-        var clickedText = clicked.innerText; 
+        if (questionIndex >= questionBox.length){
+            endGame();
+        }
+        else {
+   
+        questionContainer.textContent = questionBox[questionIndex].q; 
 
-    for(var i =0; i<questionBox.length; i++){
-        console.log(questionBox[i]);
+        aOne.textContent= questionBox[questionIndex].choices[0];
+        aTwo.textContent= questionBox[questionIndex].choices[1];
+        aThree.textContent= questionBox[questionIndex].choices[2];
+        aFour.textContent= questionBox[questionIndex].choices[3];
 
-        aOne.textContent= questionBox[i].choices[0];
-        aTwo.textContent= questionBox[i].choices[1];
-        aOne.textContent= questionBox[i].choices[2];
-        aOne.textContent= questionBox[i].choices[3];
-       
-        if (aOne.innerText === questionBox[i].a ||
-            aTwo.innerText === questionBox[i].a ||
-            aThree.innerText === questionBox[i].a ||
-            aFour.innerText === questionBox[i].a) 
+        questionIndex++;
+    }        
+   
+};
+
+    function checkAnswer() {
+
+    console.log(aOne.innerText);
+        if (aOne.innerText == questionBox[questionIndex].a ||
+            aTwo.innerText == questionBox[questionIndex].a ||
+            aThree.innerText == questionBox[questionIndex].a ||
+            aFour.innerText == questionBox[questionIndex].a) 
+
+
             {
-                var resultEl=document.createElement("div")
+                var resultEl=document.getElementById("result");
                     resultEl.textContent = "You are correct!";
-                    questionBox.appendChild(resultEl);
-        
                     score++;  
                     console.log(score);    
             }
             else{
-                resultEl.innerText = "You are wrong!";
-                questionBox.appendChild(resultEl);
+                var resultEl=document.getElementById("result");
+                resultEl.textContent = "You are wrong!";
                 timeLeft -=5;
                 console.log(timeLeft); 
-            }    
-            
-    
-      
-
-    }
-
-    aOne.addEventListener("click", question);  
-    aTwo.addEventListener("click",question)
-    aThree.addEventListener("click", question); 
-    aFour.addEventListener("click", question); 
-
-};
+            } 
+            question(); 
+    }  
 
    
+aOne.addEventListener("click", checkAnswer);  
+aTwo.addEventListener("click",checkAnswer)
+aThree.addEventListener("click", checkAnswer); 
+aFour.addEventListener("click", checkAnswer); 
+
+
+function endGame () {
+    timeLeft = 0;
+    questionIndex = 0;
+    aOne.textContent = "";
+    aTwo.textContent = "";
+    aThree.textContent = "";
+    aFour.textContent = "";
+    questionContainer.textContent = "";
+}
 
 
 var initals;
